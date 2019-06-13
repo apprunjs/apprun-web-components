61cfb06803b8ee2d970e751811405d962705c2ff
Yiyi Sun <yiyisun@gmail.com>
2019-06-13 14:28:44 -0400

    add umd

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,31 @@ import { terser } from 'rollup-plugin-terser';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import nodeResolve from "rollup-plugin-node-resolve";
 
-export default {
+export default [{
   input: 'bin/index.js',
   output: {
     file: 'public/index.js',
-    format: 'esm',
-    sourcemap: true
+    format: 'umd',
+    sourcemap: true,
+    name: "my-lib"
   },
   plugins: [
     nodeResolve(),
     terser({warnings: true, module: true, mangle: { properties: { regex: /^__/ }}}),
     sourcemaps(),
   ]
-};
+},
+{
+  input: 'bin/index.js',
+  output: {
+    file: 'public/index.esm.js',
+    format: 'esm',
+    sourcemap: true
+  },
+  plugins: [
+    nodeResolve(),
+    terser({ warnings: true, module: true, mangle: { properties: { regex: /^__/ } } }),
+    sourcemaps()
+  ]
+}
+]
